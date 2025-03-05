b24e01764952bf12a5a5056667731e66f32e9433
dauduchieu <dauduchieu.ddh775@gmail.com>
2025-03-05 21:25:06 +0700

    loading page

---
+++ style.js
@@ -0,0 +1,24 @@
+const s_textWrapper = document.querySelector('.text-wrapper')
+const s_bar = document.querySelector('.bar')
+const s_barInner = document.querySelector('.bar .inner')
+
+const s_loadingBarTime = 3000
+let s_loadingInterval = null
+
+s_textWrapper.addEventListener('mouseenter', () => {
+    s_barInner.style.width = '100%'
+    s_barInner.style.transition = `${s_loadingBarTime}ms linear`
+    s_loadingInterval = setTimeout(() => {
+        console.log('done')
+    }, s_loadingBarTime)
+})
+
+s_textWrapper.addEventListener('mouseleave', () => {
+    s_barInner.style.transition = `none`
+    s_barInner.style.width = '0%'
+    if (s_loadingInterval) {
+        clearTimeout(s_loadingInterval)
+        s_loadingInterval = null
+    }
+})
+
