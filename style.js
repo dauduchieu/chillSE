0fca54410d15e9659acfb6ed1fac1dbad56ecaf7
dauduchieu <dauduchieu.ddh775@gmail.com>
2025-03-05 21:55:56 +0700

    fix loading page, add page transition

--- style.js
+++ style.js
@@ -1,8 +1,11 @@
+const s_loadingPage = document.querySelector('.page.loading')
+const s_loginPage = document.querySelector('.page.login')
+
 const s_textWrapper = document.querySelector('.text-wrapper')
 const s_bar = document.querySelector('.bar')
 const s_barInner = document.querySelector('.bar .inner')
 
-const s_loadingBarTime = 3000
+const s_loadingBarTime = 2000
 let s_loadingInterval = null
 
 s_textWrapper.addEventListener('mouseenter', () => {
@@ -10,6 +13,8 @@ s_textWrapper.addEventListener('mouseenter', () => {
     s_barInner.style.transition = `${s_loadingBarTime}ms linear`
     s_loadingInterval = setTimeout(() => {
         console.log('done')
+        s_loadingPage.classList.add('hidden')
+        s_loginPage.classList.remove('hidden')
     }, s_loadingBarTime)
 })
 
